2c2b150eb6cbabac0ed6413dff0939857ad2d818
Brian Jeon <brian@brianjeon.com>
2019-04-07 14:00:15 -0700

    Add home and typing-test components

---
+++ src/app/typing-test/typing-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-test',
+  templateUrl: './typing-test.component.html',
+  styleUrls: ['./typing-test.component.less']
+})
+export class TypingTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
